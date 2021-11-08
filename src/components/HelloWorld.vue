0f8ec0a0d61c90cd02ee51456c4f97ec1faf8c2a
9527 <870370470@qq.com>
2021-11-08 21:25:06 +0800

    初始化Vuex

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,5 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-// eslint-disable-next-line no-undef
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ props.msg }}</h1>
 
   <p>
     Recommended IDE setup:
@@ -50,7 +41,13 @@ const count = ref(0)
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
+<script lang="ts" setup>
+import { ref } from 'vue'
 
+const props = defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
 <style scoped>
 a {
   color: #42b983;
---
+++ src/views/home/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>首页</h1>
+  <div>{{ $store.state.count }}</div>
+</template>
+
+<script lang='ts' setup></script>
+
+<style lang='scss' scoped></style>
