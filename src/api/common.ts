07a6f1fbf7af7791bd034556ebb2adb8e4a35d11
9527 <870370470@qq.com>
2021-11-12 22:21:33 +0800

    规范环境变量、跨域配置

--- src/api/common.ts
+++ src/api/common.ts
@@ -10,7 +10,7 @@ import { ILoginInfo } from './types/common'
 export const getLoginInfo = () => {
   return request<ILoginInfo>({
     method: 'GET',
-    url: '/login/info'
+    url: '/admin/login/info'
   })
   // return request.get<ResponseData<{
   //   login_logo:string
--- vite.config.ts
+++ vite.config.ts
@@ -24,5 +24,23 @@ export default defineConfig({
         additionalData: '@import "@/styles/variables.scss";'
       }
     }
+  },
+  server: {
+    proxy: {
+      // 字符串简写写法
+      // '/foo': 'http://localhost:4567',
+      // 选项写法
+      '/admin': {
+        target: 'https://shop.fed.lagou.com/api',
+        changeOrigin: true
+        // 兼容基于名字的虚拟主机
+        // a.com localhost:xxx
+        // b.com localhost:xxx
+        // HTTP 请求头部的 origin 字段
+        // 我们在开发模式：默认的origin是真实的origin localhost:3000
+        //  changeOrigin: true 代理服务会把origin 修改为目标地址
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
